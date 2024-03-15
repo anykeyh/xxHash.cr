267ce1aab5e74f942944a8a38bab230d548de155
Tom Sawyer <tsawyer@nationalaperture.com>
2024-03-15 15:49:21 -0400

    Literal type specifier is required.

--- spec/xx_hash_spec.cr
+++ spec/xx_hash_spec.cr
@@ -1,30 +1,30 @@
 require "./spec_helper"
-require "../src/xx_hash"
+require "../spec/xx_hash"
 
 CASES = [
   # Case A > 32 characters fill buff for 32 and 64 bits versions
-  {"Nobody inspects the spammish repetition", 0, 0xe2293b2f, 0xfbcea83c8a378bf1},
-  {"Nobody inspects the spammish repetition", 1234, 0x298ce4e5, 0x55e30cd248a5419f}, #https://asecuritysite.com/encryption/xxHash
+  {"Nobody inspects the spammish repetition", 0, 0xe2293b2f, 0xfbcea83c8a378bf1_u64},
+  {"Nobody inspects the spammish repetition", 1234, 0x298ce4e5, 0x55e30cd248a5419f_u64}, #https://asecuritysite.com/encryption/xxHash
 
   # Case B < 16 characters no buffer filled
-  {"small", 0, 0x8e52cd6a, 0xa8ce38f99756b7b3},
-  {"small", 1234, 0x1acf736b, 0x3709561c2893a1d0},
+  {"small", 0, 0x8e52cd6a, 0xa8ce38f99756b7b3_u64},
+  {"small", 1234, 0x1acf736b, 0x3709561c2893a1d0_u64},
 
   # Case C empty string
-  {"", 0, 0x02cc5d05, 0xef46db3751d8e999},
-  {"", 1234, 0xb625a4db, 0xe80dc1ad52e210e4},
+  {"", 0, 0x02cc5d05, 0xef46db3751d8e999_u64},
+  {"", 1234, 0xb625a4db, 0xe80dc1ad52e210e4_u64},
 
   # Case D a long string
   {"Cras dignissim sem sed euismod pretium. Sed facilisis diam dolor. Curabitur turpis ex, euismod vel est ut, pharetra condimentum lacus. Sed eget aliquet tellus, in vestibulum tellus. Duis tellus tortor, porttitor nec arcu quis, tristique lobortis neque. Cras pharetra, sem in vestibulum mollis, dui elit vulputate nulla, eget egestas elit tellus in leo. Proin fermentum eros nec dolor dictum rutrum. Curabitur quam lacus, posuere non turpis sit amet, gravida molestie augue. Vestibulum nec venenatis purus. Pellentesque vestibulum nunc ut libero pharetra, ut egestas odio hendrerit. Duis euismod ipsum leo, id lacinia massa ultricies eu. In congue venenatis tristique. In ullamcorper augue quam, id pretium ex dictum id. Morbi blandit eleifend neque, quis pulvinar neque dignissim at. Integer sodales diam ac metus maximus, sed cursus risus commodo.", 0, 0xbc702865, 0x27b8f6a015650fb5 },
   {"Cras dignissim sem sed euismod pretium. Sed facilisis diam dolor. Curabitur turpis ex, euismod vel est ut, pharetra condimentum lacus. Sed eget aliquet tellus, in vestibulum tellus. Duis tellus tortor, porttitor nec arcu quis, tristique lobortis neque. Cras pharetra, sem in vestibulum mollis, dui elit vulputate nulla, eget egestas elit tellus in leo. Proin fermentum eros nec dolor dictum rutrum. Curabitur quam lacus, posuere non turpis sit amet, gravida molestie augue. Vestibulum nec venenatis purus. Pellentesque vestibulum nunc ut libero pharetra, ut egestas odio hendrerit. Duis euismod ipsum leo, id lacinia massa ultricies eu. In congue venenatis tristique. In ullamcorper augue quam, id pretium ex dictum id. Morbi blandit eleifend neque, quis pulvinar neque dignissim at. Integer sodales diam ac metus maximus, sed cursus risus commodo.", 1234, 0x0f8da06d, 0x1cf0478f6499f2ae },
 
   # Case E exactly 16 bytes
-  {"1234567890abcdef", 0, 0x17fddd25, 0x1b576a0cfae1707e },
-  {"1234567890abcdef", 1234, 0x3c299461, 0x0a176fa30c839a45 },
+  {"1234567890abcdef", 0, 0x17fddd25, 0x1b576a0cfae1707e_u64 },
+  {"1234567890abcdef", 1234, 0x3c299461, 0x0a176fa30c839a45_u64 },
 
   # Case F exactly 32 bytes
-  {"1234567890abcdef1234567890abcdef", 0, 0xf11e8d8c, 0xc1bf3856a435f3ec },
-  {"1234567890abcdef1234567890abcdef", 1234, 0x80105224, 0x3e450250700580a0 },
+  {"1234567890abcdef1234567890abcdef", 0, 0xf11e8d8c, 0xc1bf3856a435f3ec_u64 },
+  {"1234567890abcdef1234567890abcdef", 1234, 0x80105224, 0x3e450250700580a0_u64 },
 
 ]
 module XXHashSpec
